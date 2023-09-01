293831b3a1437a75ee11e081491fd1f00775354b
Tedy Warsitha <tedy@warsitha.se>
2023-09-01 11:52:39 +0200

    Minify

--- webpack.config.js
+++ webpack.config.js
@@ -33,8 +33,5 @@ module.exports = {
     resolve: {
         extensions: ['.ts', '.tsx', '.js']
     },
-    externals: [nodeExternals()],
-    optimization: {
-        minimize: false
-    }
+    externals: [nodeExternals()]
 };
